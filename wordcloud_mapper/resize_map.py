11b5871c3eb74677058aac446c764b33f2714819
Gabriel da Silva Zech <gabrielszech@gmail.com>
2022-07-19 16:31:22 +0200

    format to pep8

--- wordcloud_mapper/resize_map.py
+++ wordcloud_mapper/resize_map.py
@@ -7,14 +7,16 @@ def resize_map(fig, scale):
     fig : matplotlib.figure.Figure
         A matplotlib Figure object.
     scale : float
-        The rescaling factor as a multiplier of the figure's current size.
-        If ``scale > 1.0``, image gets bigger by a factor of scale (e.g. 1.5 means 50% bigger).
-        If ``scale < 1.0``, image gets smaller by a factor of scale (e.g. 0.5 means 50% smaller).
-        If ``scale = 1.0``, no change is made.
+        The rescaling factor as a multiplier of the figure's current size. If
+        ``scale > 1.0``, image gets bigger by a factor of scale (e.g. 1.5 means
+        50% bigger). If ``scale < 1.0``, image gets smaller by a factor of
+        scale (e.g. 0.5 means 50% smaller). If ``scale = 1.0``, no change is
+        made.
 
     """
     # get current size and multiply that by the given scale
     width, height = fig.get_size_inches()*scale
     fig.set_size_inches(width, height)
     dpi = fig.dpi
-    print(f"Figure resized to width {int(width*dpi)}px and height {int(height*dpi)}px.")
+    print(f"Figure resized to width {int(width*dpi)}px\
+         and height {int(height*dpi)}px.")
--- wordcloud_mapper/wordcloud_map.py
+++ wordcloud_mapper/wordcloud_map.py
@@ -16,27 +16,31 @@ def download_shapefiles(map_scale="10M",
     Parameters
     ----------
     map_scale : str (default = "10M")
-        Scale used for the regions’ polygon shapes (i.e. regional boundaries).
-        Smaller scales (e.g. "03M") mean more detailed polygon shapes and thus longer running times.
-        Larger scales (e.g. "60M") mean less detailed polygon shapes and thus shorter running times.
-        Available values: ``"60M"``, ``"20M"``, ``"10M"``, ``"03M"`` or ``"01M"``.
-        For a visual explanation, see https://raw.githubusercontent.com/ropengov/giscoR/master/img/README-example-1.png
+        Scale used for the regions' polygon shapes (i.e. regional boundaries).
+        Smaller scales (e.g. "03M") mean more detailed polygon shapes and thus
+        longer running times. Larger scales (e.g. "60M") mean less detailed
+        polygon shapes and thus shorter running times. Available values:
+        ``"60M"``, ``"20M"``, ``"10M"``, ``"03M"`` or ``"01M"``. For a visual
+        explanation, see
+        https://raw.githubusercontent.com/ropengov/giscoR/master/img/README-example-1.png
     year : int (default = 2021)
         The year of NUTS regulation, e.g. 2021, 2016, 2013, 2010, 2006 or 2003.
     coord_system : int (default = 4326)
-        4-digit EPSG code (a unique identifier for different coordinate systems).
-        Available values:
-        ``4326`` (WGS84, coordinates in decimal degrees),
-        ``3035`` (ETRS 1989 in Lambert Azimutal projection with centre in E52N10, coordinates in meters),
-        ``3857`` (WGS84 Web Mercator Auxiliary Sphere, coordinates in meters).
+        4-digit EPSG code (a unique identifier for different coordinate
+        systems). Available values: ``4326`` (WGS84, coordinates in decimal
+        degrees), ``3035`` (ETRS 1989 in Lambert Azimutal projection with
+        centre in E52N10, coordinates in meters), ``3857`` (WGS84 Web Mercator
+        Auxiliary Sphere, coordinates in meters).
 
     Returns
     -------
     shapefile.Reader
-        Reader object of shapefile module containing shapefiles of NUTS regions.
+        Reader object of shapefile module containing shapefiles of NUTS
+        regions.
 
     """
-    url = f"https://gisco-services.ec.europa.eu/distribution/v2/nuts/shp/NUTS_RG_{map_scale}_{year}_{coord_system}.shp.zip"
+    url = f"https://gisco-services.ec.europa.eu/distribution/v2/nuts/shp/\
+        NUTS_RG_{map_scale}_{year}_{coord_system}.shp.zip"
     shapefiles = Reader(url)
 
     return shapefiles
@@ -68,12 +72,14 @@ def get_unique_codes(df,
 def get_bbox_map(shapefiles,
                  unique_codes):
     """
-    Retreive the minimum and maximum X and Y bounding box values from all shapefiles to use as boundaries for the final wordcloud map.
+    Retreive the minimum and maximum X and Y bounding box values from all
+    shapefiles to use as boundaries for the final wordcloud map.
 
     Parameters
     ----------
     shapefiles : shapefile.Reader
-        Reader object of shapefile module containing shapefiles of NUTS regions.
+        Reader object of shapefile module containing shapefiles of NUTS
+        regions.
     unique_codes : ndarray
         Array containing unique NUTS codes.
 
@@ -112,12 +118,14 @@ def get_bbox_map(shapefiles,
 
 def get_bbox_region(shaperecord):
     """
-    Retreive the minimum and maximum X and Y bounding box values for a given region.
+    Retreive the minimum and maximum X and Y bounding box values for a given
+    region.
 
     Parameters
     ----------
     shaperecord : shapefile.ShapeRecord
-        ShapeRecord object of shapefile module containing information about a single NUTS region.
+        ShapeRecord object of shapefile module containing information about a
+        single NUTS region.
 
     Returns
     -------
@@ -141,20 +149,23 @@ def get_mask(shaperecord,
              contour_colour="black",
              resolution=100):
     """
-    Create images to be used as mask (i.e. the shape) of the wordcloud and to add border lines to the final wordcloud map.
+    Create images to be used as mask (i.e. the shape) of the wordcloud and to
+    add border lines to the final wordcloud map.
 
     Parameters
     ----------
     shaperecord : shapefile.ShapeRecord
-        ShapeRecord object of shapefile module containing information about a single NUTS region.
+        ShapeRecord object of shapefile module containing information about a
+        single NUTS region.
     fill_colour : str (default = "black")
-        Fill colour used when creating mask of region.
-        Value None is passed when adding borders of regions to final wordcloud map.
+        Fill colour used when creating mask of region. Value None is passed
+        when adding borders of regions to final wordcloud map.
     contour_colour : str (default = "black")
-        Contour colour to use when showing regional borders in final wordcloud map.
+        Contour colour to use when showing regional borders in final wordcloud
+        map.
     resolution : str (default = 100)
-        DPI (dots per inch) value used to generate mask image.
-        Higher values create sharper regional border lines but take longer to run.
+        DPI (dots per inch) value used to generate mask image. Higher values
+        create sharper regional border lines but take longer to run.
 
     Returns
     -------
@@ -171,7 +182,7 @@ def get_mask(shaperecord,
 
     # plot region
     geometry = shaperecord.__geo_interface__["geometry"]
-    if fill_colour == None:
+    if fill_colour is None:
         # used to plot region borders
         ax.add_patch(PolygonPatch(geometry, fill=False,
                      ec=contour_colour, alpha=0.3))
@@ -182,7 +193,7 @@ def get_mask(shaperecord,
     # save image in memory buffer and reshape
     io_buf = BytesIO()
     fig.savefig(io_buf, format='raw', dpi=resolution, transparent=True)
-    width, height = fig.get_size_inches()*resolution
+    width, height = fig.get_size_inches() * resolution
     io_buf.seek(0)
     img_arr = frombuffer(io_buf.getvalue(), dtype=uint8)
     img_arr = reshape(img_arr, newshape=(int(height), int(width), -1))
@@ -200,7 +211,8 @@ def get_data(df,
              word_counts,
              nuts_code):
     """
-    For a given NUTS code, retreive its words and their count/frequency from the DataFrame containing all data.
+    For a given NUTS code, retreive its words and their count/frequency from
+    the DataFrame containing all data.
 
     Parameters
     ----------
@@ -218,7 +230,8 @@ def get_data(df,
     Returns
     -------
     dict
-        Dictionary containing the words as keys and their count as values for the given NUTS region.
+        Dictionary containing the words as keys and their count as values for
+        the given NUTS region.
 
     """
     df_temp = df.loc[df[nuts_codes] == nuts_code]
@@ -246,37 +259,48 @@ def plot_region(mask,
     mask : ndarray
         The image array to use as mask (i.e. shape) of the wordcloud.
     data : dict
-        Dictionary containing the words as keys and their count as values for the given NUTS region.
+        Dictionary containing the words as keys and their count as values for
+        the given NUTS region.
     ax : matplotlib.Axes class
-        An instance of the matplotlib Axes class to which the wordcloud is plotted to.
+        An instance of the matplotlib Axes class to which the wordcloud is
+        plotted to.
     bbox : tuple
-        Tuple containing (minX, maxX, minY, maxY) bounding box values of the region.
+        Tuple containing (minX, maxX, minY, maxY) bounding box values of the
+        region.
     colour_func : str (default = "random")
-        String indicating which colour function to use.
-        Available values:
+        String indicating which colour function to use. Available values:
         ``"random"`` sets a random luminosity to each word within a region,
-        ``"frequency"`` sets the luminosity of each word according to their frequency/word count, where the most frequent word receives the max luminosity of 50.
+        ``"frequency"`` sets the luminosity of each word according to their
+        frequency/word count, where the most frequent word receives the max
+        luminosity of 50.
     rendering_quality : int (default = 1)
-        The rendering quality of the words in the wordcloud.
-        Higher values produce better-looking / sharper words but take longer to run.
+        The rendering quality of the words in the wordcloud. Higher values
+        produce better-looking / sharper words but take longer to run.
     min_font_size : int (default = 4)
-        Smallest font size to use. Word placement will stop when there is no more room to fit words of this size.
+        Smallest font size to use. Word placement will stop when there is no
+        more room to fit words of this size.
     max_font_size : int or None (default = None)
-        Maximum font size for the largest word. If None, a relative sizing based on the height of the image is used.
+        Maximum font size for the largest word. If None, a relative sizing
+        based on the height of the image is used.
     max_words : int (default = 200)
         Maximum number of words to be included in wordcloud for each region.
     relative_scaling : float (default = 'auto')
-        Importance of relative word frequencies for font-size.
-        With ``relative_scaling = 0``, only the ranking of words is considered.
-        With ``relative_scaling = 1``, a word that is twice as frequent will have twice the size.
-        In datasets with highly uneven word frequencies, relative_scaling = 1 might lead to very few words being fitted, so a value of around 0.5 often looks better.
-        If ``relative_scaling = 'auto'`` it will be set to 0.5 unless ``repeat = True``, in which case it will be set to 0.
+        Importance of relative word frequencies for font-size. With
+        ``relative_scaling = 0``, only the ranking of words is considered. With
+        ``relative_scaling = 1``, a word that is twice as frequent will have
+        twice the size. In datasets with highly uneven word frequencies,
+        relative_scaling = 1 might lead to very few words being fitted, so a
+        value of around 0.5 often looks better. If ``relative_scaling =
+        'auto'`` it will be set to 0.5 unless ``repeat = True``, in which case
+        it will be set to 0.
     prefer_horizontal : float (default = 0.9)
-        The ratio of times to try horizontal fitting as opposed to vertical.
-        If ``prefer_horizontal = 1``, no words will be placed vertically.
-        If ``prefer_horizontal < 1``, the algorithm will try rotating the word if it doesn’t fit.
+        The ratio of times to try horizontal fitting as opposed to vertical. If
+        ``prefer_horizontal = 1``, no words will be placed vertically. If
+        ``prefer_horizontal < 1``, the algorithm will try rotating the word if
+        it doesn’t fit.
     repeat : bool (default = False)
-        Whether to repeat already-placed words until ``max_words`` or ``min_font_size`` is reached.
+        Whether to repeat already-placed words until ``max_words`` or
+        ``min_font_size`` is reached.
 
     """
     # set a random hue colour
@@ -303,16 +327,16 @@ def plot_region(mask,
             String containing HSL colour values.
 
         """
-        h = hue
-        s = 100
-        l = randint(25, 50)  # prevents being too bright or too dark
+        sat = 100
+        lum = randint(25, 50)  # prevents being too bright or too dark
 
-        return "hsl({}, {}%, {}%)".format(h, s, l)
+        return "hsl({}, {}%, {}%)".format(hue, sat, lum)
 
     def colour_func_frequency(word,
                               **kwargs):
         """
-        Set the luminosity of each word according to their frequency/word count, where the most frequent word receives the max luminosity of 50.
+        Set the luminosity of each word according to their frequency/word
+        count, where the most frequent word receives the max luminosity of 50.
 
         Parameters
         ----------
@@ -331,15 +355,15 @@ def plot_region(mask,
 
         """
         # normalise frequencies
-        data_norm = (fromiter(data.values(), dtype=float) -
-                     min(data.values())) / (max(data.values()) - min(data.values()))
+        data_norm = (fromiter(data.values(), dtype=float)
+                     - min(data.values())) \
+            / (max(data.values()) - min(data.values()))
         word_index = list(data.keys()).index(word)
 
-        h = hue
-        s = 100
-        l = 50 * data_norm[word_index]
+        sat = 100
+        lum = 50 * data_norm[word_index]
 
-        return "hsl({}, {}%, {}%)".format(h, s, l)
+        return "hsl({}, {}%, {}%)".format(hue, sat, lum)
 
     # set colour function according to user input
     if colour_func == "random":
@@ -349,7 +373,7 @@ def plot_region(mask,
 
     # create wordcloud
     wc = WordCloud(mask=mask,
-                   background_color=None, mode="RGBA",  # creates transparent background
+                   background_color=None, mode="RGBA",  # transp background
                    scale=rendering_quality,
                    color_func=func,
                    min_font_size=min_font_size,
@@ -374,14 +398,17 @@ def plot_contour(shaperecord, ax, bbox, resolution=100):
     Parameters
     ----------
     shaperecord : shapefile.ShapeRecord
-        ShapeRecord object of shapefile module containing information about a single NUTS region.
+        ShapeRecord object of shapefile module containing information about a
+        single NUTS region.
     ax : matplotlib.Axes class
-        An instance of the matplotlib Axes class to which the wordcloud is plotted to.
+        An instance of the matplotlib Axes class to which the wordcloud is
+        plotted to.
     bbox : tuple
-        Tuple containing (minX, maxX, minY, maxY) bounding box values of the region.
+        Tuple containing (minX, maxX, minY, maxY) bounding box values of the
+        region.
     resolution : str (default = 100)
-        DPI (dots per inch) value used to generate mask image.
-        Higher values create sharper regional border lines but take longer to run.
+        DPI (dots per inch) value used to generate mask image. Higher values
+        create sharper regional border lines but take longer to run.
 
     """
     contour = get_mask(shaperecord, fill_colour=None,
@@ -417,37 +444,45 @@ def wordcloud_map(df,
     word_counts : str
         Name of the column in the DataFrame containing the word counts.
     colour_func : str (default = "random")
-        String indicating which colour function to use.
-        Available values:
+        String indicating which colour function to use. Available values:
         ``"random"`` sets a random luminosity to each word within a region,
-        ``"frequency"`` sets the luminosity of each word according to their frequency/word count, where the most frequent word receives the max luminosity of 50.
+        ``"frequency"`` sets the luminosity of each word according to their
+        frequency/word count, where the most frequent word receives the max
+        luminosity of 50.
     scale : float (default = 1)
-        The scale of the produced figure.
-        The given value works as a multiplier of matplotlib's default figure size.
-        If ``scale = 1.0``, retains default figure size.
-        If ``scale > 1.0``, figure gets bigger by a factor of scale (e.g. 1.5 means 50% bigger).
-        If ``scale < 1.0``, figure gets smaller by a factor of scale (e.g. 0.5 means 50% smaller).
+        The scale of the produced figure. The given value works as a multiplier
+        of matplotlib's default figure size. If ``scale = 1.0``, retains
+        default figure size. If ``scale > 1.0``, figure gets bigger by a factor
+        of scale (e.g. 1.5 means 50% bigger). If ``scale < 1.0``, figure gets
+        smaller by a factor of scale (e.g. 0.5 means 50% smaller).
     rendering_quality : int (default = 1)
-        The rendering quality of the words in the wordcloud.
-        Higher values produce better-looking / sharper words but take longer to run.
+        The rendering quality of the words in the wordcloud. Higher values
+        produce better-looking / sharper words but take longer to run.
     min_font_size : int (default = 4)
-        Smallest font size to use. Word placement will stop when there is no more room to fit words of this size.
+        Smallest font size to use. Word placement will stop when there is no
+        more room to fit words of this size.
     max_font_size : int or None (default = None)
-        Maximum font size for the largest word. If None, a relative sizing based on the height of the image is used.
+        Maximum font size for the largest word. If None, a relative sizing
+        based on the height of the image is used.
     max_words : int (default = 200)
         Maximum number of words to be included in wordcloud for each region.
     relative_scaling : float (default = 'auto')
-        Importance of relative word frequencies for font-size.
-        With ``relative_scaling = 0``, only the ranking of words is considered.
-        With ``relative_scaling = 1``, a word that is twice as frequent will have twice the size.
-        In datasets with highly uneven word frequencies, relative_scaling = 1 might lead to very few words being fitted, so a value of around 0.5 often looks better.
-        If ``relative_scaling = 'auto'`` it will be set to 0.5 unless ``repeat = True``, in which case it will be set to 0.
+        Importance of relative word frequencies for font-size. With
+        ``relative_scaling = 0``, only the ranking of words is considered. With
+        ``relative_scaling = 1``, a word that is twice as frequent will have
+        twice the size. In datasets with highly uneven word frequencies,
+        relative_scaling = 1 might lead to very few words being fitted, so a
+        value of around 0.5 often looks better. If ``relative_scaling =
+        'auto'`` it will be set to 0.5 unless ``repeat = True``, in which case
+        it will be set to 0.
     prefer_horizontal : float (default = 0.9)
-        The ratio of times to try horizontal fitting as opposed to vertical.
-        If ``prefer_horizontal = 1``, no words will be placed vertically.
-        If ``prefer_horizontal < 1``, the algorithm will try rotating the word if it doesn’t fit.
+        The ratio of times to try horizontal fitting as opposed to vertical. If
+        ``prefer_horizontal = 1``, no words will be placed vertically. If
+        ``prefer_horizontal < 1``, the algorithm will try rotating the word if
+        it doesn't fit.
     repeat : bool (default = False)
-        Whether to repeat already-placed words until ``max_words`` or ``min_font_size`` is reached.
+        Whether to repeat already-placed words until ``max_words`` or
+        ``min_font_size`` is reached.
 
     Returns
     -------
@@ -478,9 +513,10 @@ def wordcloud_map(df,
 
     # get current size and multiply that by the given scale
     width, height = fig.get_size_inches()
-    fig.set_size_inches(width*scale, height*scale)
+    fig.set_size_inches(width * scale, height * scale)
     dpi = fig.dpi
     print(
-        f"Figure successfully produced with width {int(width*dpi)}px and height {int(height*dpi)}px.")
+        f"Figure successfully produced with width {int(width*dpi)}px\
+             and height {int(height*dpi)}px.")
 
     return fig
